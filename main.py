506d33be28163ae2fcdc301bd627eebd2117656a
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-02-25 14:51:19 +0100

    -date validation

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from datetime import datetime
 import pyttsx3
 from calculatefuture import algocal
 
+
 def userchoice():
     userag = True
     print("WELCOME!!! LETS SEE YOUR FUTURE.\n")
-    pyttsx3.speak("WELCOME!!! LETS SEE YOUR FUTURE.")
+    # pyttsx3.speak("WELCOME!!! LETS SEE YOUR FUTURE.")
     while userag:  # if user want to perform the activity again
         userdate = str(input('Enter date(yyyy-mm-dd): '))
-        stripdate = datetime.strptime(userdate, "%Y-%m-%d")
-        #print(stripdate.year)
-        x = algocal(stripdate)
-        x.datecal()
+        try:
+            classobj = algocal(userdate)  # calling the class
+            classobj.validate()
+        except ValueError:
+            print("Enter date in proper format.")
+        # print(stripdate.year)
+        loopchoice = str(input('\nIf you want to revisit then press "Y". If you want to exit press "N". ').upper())
+        if loopchoice == "Y":
+            userag = True
+        else:
+            userag = False
+
 
 userchoice()
