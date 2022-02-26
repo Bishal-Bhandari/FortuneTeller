d037019485d813c8a6bf60d6a5014a307464df66
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-02-26 14:18:23 +0100

    -characters added as per the user date

--- calculatefuture.py
+++ calculatefuture.py
@@ -21,15 +21,19 @@ class AlgoCal:
         excelyear = pd.read_excel(r'dateyear.xlsx')  # read file for year
         excelmonth = pd.read_excel(r'datemonth.xlsx')  # read file for month
         yearver = excelyear['year'].values  # reading year column from file
+        yearcharver = excelyear['yearchar'].values  # reading year char column from file
         monthver = excelmonth['months'].values  # reading month column from file
-        self.fortunecal(datemonth, dateyear, yearver, monthver)
+        monthcharver = excelmonth['charmonth'].values  # reading month column from file
+        self.fortunecal(datemonth, dateyear, yearver, monthver, yearcharver, monthcharver)
 
-    def fortunecal(self, datemonth, dateyear, yearver, monthver):
+    def fortunecal(self, datemonth, dateyear, yearver, monthver, yearcharver, monthcharver):
         for i in range(len(monthver)):  # for looping on basis of month
             if monthver[i] == datemonth:  # for checking the user month
                 for j in range(len(yearver)):  # for looping on basis of year
                     try:
                         if yearver[j] == dateyear:  # for checking the user year
-                            print(yearver[j])
+                            print("\n Your personal characters are as follow:")
+                            print("\nAccording to solar cycle your characters are:\n" + yearcharver[j])
+                            print("\nAccording to lunar cycle your characters are:\n" + monthcharver[i])
                     except ValueError:
                         print("No valid date found.")
